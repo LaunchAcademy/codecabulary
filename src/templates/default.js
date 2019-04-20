e72e7f404157d50a10fdb5b63e819620ef654bab
Dan Pickett <dan.pickett@launchacademy.com>
2019-04-20 14:08:35 -0400

    remove irrelevant item from frontmatter expectation

--- src/templates/default.js
+++ src/templates/default.js
@@ -10,7 +10,6 @@ export default function Template({
     <div className="blog-post-container">
       <div className="blog-post">
         <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
         <div
           className="blog-post-content"
           dangerouslySetInnerHTML={{ __html: html }}
@@ -20,15 +19,14 @@ export default function Template({
   )
 }
 
-// export const pageQuery = graphql`
-//   query($path: String!) {
-//     markdownRemark(frontmatter: { path: { eq: $path } }) {
-//       html
-//       frontmatter {
-//         date(formatString: "MMMM DD, YYYY")
-//         path
-//         title
-//       }
-//     }
-//   }
-//   `
+export const pageQuery = graphql`
+  query($path: String!) {
+    markdownRemark(frontmatter: { path: { eq: $path } }) {
+      html
+      frontmatter {
+        path
+        title
+      }
+    }
+  }
+  `
